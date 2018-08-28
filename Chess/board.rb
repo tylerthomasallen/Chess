f44cbfe35f34432509e5f2b194258d28f4876fc3
tylerthomasallen <tylerthomasallen@gmail.com>
2018-08-28 11:14:10 -0700

    Find correct path of Chess piece to ending position

--- Chess/board.rb
+++ Chess/board.rb
@@ -10,30 +10,69 @@ class Board
   end
   
   def move_piece(start_pos,new_pos)
+    
     starting_row, starting_col = start_pos
     new_row, new_col = new_pos
-    start_piece = get_piece(starting_row,starting_col)
+    start_piece = @grid[starting_row][starting_col]
     raise "No pieces at given position, try again." if start_piece.is_a?(NullPiece)
     
-    if valid_move?(start_piece,start_pos,new_pos)
-      move_to(start_piece,new_pos)
-    else
-      raise ""
+    possible_moves = start_piece.possible_moves(start_pos)
+    unless possible_moves.include?(new_pos) && ((0..7).to_a.include?(new_row) && (0..7).to_a.include?(new_col))
+      raise 'Invalid movement'
+    end
+    
+    
+    # if valid_move?(start_pos,new_pos)
+    #   move_to(start_piece,new_pos)
+    # else
+    #   raise ""
+    # end
   end
   
-  def move_to(piece,pos)
+  def get_base_move(start_pos,ending_pos)
+    path = []
+    x_dif = ending_pos[0] - start_pos[0]
+    y_dif = ending_pos[1] - start_pos[1]
+    
+    difs = [x_dif, y_dif]
+    start_piece = @grid[start_pos[0]][start_pos[1]]
+    base_moves = start_piece.base_moves
+    
+    correct_move = base_moves.select do |move|
+      x_mov = x_dif * move[0]
+      y_mov = y_dif * move[1]
+      x_mov >= 0 && y_mov >= 0 && x_mov + y_mov > 0
+    end
+    
+    correct_move
+    
   end
   
-  def [](pos)
-    row,col = pos
-    @grid[row][col]
-  end
   
-  def []=(pos, target)
-    row, col = pos
-    @grid[row][col] = target
-  end
   
+  
+  
+  # def move_to(piece,pos)
+  # end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  # def [](pos)
+  #   row,col = pos
+  #   @grid[row][col]
+  # end
+  # 
+  # def []=(pos, target)
+  #   row, col = pos
+  #   @grid[row][col] = target
+  # end
+  # 
   
   
   private
@@ -53,7 +92,7 @@ class Board
       end
 
       starting_grid.length.times do |col|
-        starting_grid[row][col] = NullPiece.new
+        starting_grid[row][col] = NullPiece.instance
       end
     end
     return starting_grid
@@ -61,22 +100,22 @@ class Board
   
   def populate_pawns(grid, row)
     # debugger
-    grid[row].map{ |cell| cell = Pawn.new }
+    grid[row].map{ |cell| cell = Pawn.new("green") }
   end
   
   def populate_back_row(grid, row)
     grid[row].length.times do |i|
       case i
       when 0, 7
-        grid[row][i] = "Rook.new"
+        grid[row][i] = Rook.new("green")
       when 1, 6
-        grid[row][i] = "Knight.new"
+        grid[row][i] = Knight.new("green")
       when 2, 5
-        grid[row][i] = "Bishop.new"
+        grid[row][i] = Bishop.new("green")
       when 3
-        grid[row][i] = "Queen.new"
+        grid[row][i] = Queen.new("green")
       when 4
-        grid[row][i] = "King.new"
+        grid[row][i] = King.new("green")
       end
       
     end
--- Chess/piece.rb
+++ Chess/piece.rb
@@ -1,14 +1,29 @@
+require 'singleton'
+
 class Piece
   attr_accessor :pos
   attr_reader :color
-  def initialize(color,pos)
+  def initialize(color)
     @color = color
-    @pos = pos
+    #@pos = pos
+  end
+  
+  def possible_moves(pos)
+    possible_moves = []
+    @moves.each do |ele|
+      possible_moves << [ele[0] + pos[0], ele[1] + pos[1]]
+    end
+    possible_moves
   end
   
 end
 
 class Knight < Piece
+  attr_reader :moves
+  def initialize(color)
+    super
+    @moves = [[1,2],[1,-2]]
+  end
 end
 
 class Pawn < Piece
@@ -20,6 +35,30 @@ class Bishop < Piece
 end
 
 class Rook < Piece
+  attr_reader :moves, :base_moves
+  def initialize(color)
+    super
+    @moves = generate_moves
+    @base_moves = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+  end
+  
+  def generate_moves
+    up = []
+    down = []
+    left = []
+    right = []
+    
+    i = 0
+    while i < 7
+      up << [i+1,0]
+      down << [-(i+1),0]
+      left << [0,-(i+1)]
+      right << [0,i+1]
+      i+=1
+    end
+    up + down + left + right
+  end
+
 end
 
 class King < Piece
@@ -30,4 +69,7 @@ end
 
 
 class NullPiece < Piece
+  include Singleton
+  def initialize
+  end
 end
